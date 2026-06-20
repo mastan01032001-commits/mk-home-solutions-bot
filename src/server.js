280d757f5943e08d0b0b3a90a6b72b395517dbe9
mastan01032001-commits <mastan01032001@gmail.com>
2026-06-21 00:19:00 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,1 +1,36 @@
-server.js
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("MK Home Solutions Bot Running");
+});
+
+app.get("/webhook", (req, res) => {
+  const verifyToken = process.env.VERIFY_TOKEN;
+
+  if (
+    req.query["hub.mode"] === "subscribe" &&
+    req.query["hub.verify_token"] === verifyToken
+  ) {
+    return res.status(200).send(req.query["hub.challenge"]);
+  }
+
+  return res.sendStatus(403);
+});
+
+app.post("/webhook", (req, res) => {
+  console.log("Incoming WhatsApp message:");
+  console.log(JSON.stringify(req.body, null, 2));
+
+  res.sendStatus(200);
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`MK Home Solutions Bot running on port ${PORT}`);
+});
